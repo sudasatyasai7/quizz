f36fe838fbe326e4f82e1b26e0281d2f2e71864e
sudasatyasai7 <satyasai9883@gmail.com>
2024-01-31 21:31:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React,{useState} from 'react'
-import questions from './question.json'
-import Display from './display'
-import './App.css'
-const App = () => {
-  let temper=[];
-  const[marks,setMarks]=useState([]);
-  const event=(value,index)=>{
-    temper[index]=value;
-    setMarks([...temper]);
+import React, { useState } from 'react';
+import questions from './question.json';
+import Display from './display';
+import './App.css';
 
-  }
+const App = () => {
+  const [marks, setMarks] = useState(Array(questions.length).fill(null));
+
+  const handleEvent = (value, index) => {
+    setMarks(prevMarks => {
+      const newMarks = [...prevMarks];
+      newMarks[index] = value;
+      return newMarks;
+    });
+  };
+
+  const calculateTotalMarks = () => {
+    const totalMarks = marks.reduce((a, b) => a + (b || 0), 0);
+    alert(totalMarks);
+  };
+
   return (
     <div>
       <center>
         <h1>Quizz</h1>
-        {questions.map((que,index)=>{
-          return(
-            <Display que={que} index={index} event={event}/>
-
-          )
-        })}
-        <button onClick={()=>alert(marks.reduce((a,b)=>a+b,0))}>End Quizz</button>
+        {questions.map((que, index) => (
+          <Display key={index} que={que} index={index} event={handleEvent} />
+        ))}
+        <button onClick={calculateTotalMarks}>End Quizz</button>
       </center>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
